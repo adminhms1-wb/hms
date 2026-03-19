c0f2a8aa41a07badc7db661cbde6820f61f1e9a9
adminhms1-wb <adminhms1@gmail.com>
2026-03-19 21:59:26 +0500

    Change and test files

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -4,7 +4,7 @@
             <div class="login-left">
                 <div class="login-brand">
                     <h1>Hotel Management System</h1>
-                    <p>Admin Dashboard Template</p>
+                    <p>Admin Dashboard Template dfgghyy333</p>
                 </div>
             </div>
             <div class="login-right">
